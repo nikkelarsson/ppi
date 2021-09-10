ae604d735d0b53de0a8d9650b310d73e63abc3b8
Niklas Mikael <nikke.larsson@gmail.com>
2021-09-10 11:18:34 +0300

    Added couple dunder -methods etc.

--- ppi/parsing.py
+++ ppi/parsing.py
@@ -7,3 +7,20 @@ Date: September 10, 2021
 class Parsing:
     def __init__(self, args: list) -> None:
         self.args: list = args
+
+        # Invalid prefix types, sequences etc. we don't want
+        # to accept as prefixes.
+        #
+        # New chars/sequences can be added here, by making
+        # own section for the sequence etc.
+        self.invalid_prefixes: dict = {
+                "too_many_hyphens": [
+                    "-" * index for index, i in enumerate(range(20), 3)
+                    ],
+                }
+
+    def __str__(self) -> str:
+        return f"Args: {self.args}."
+
+    def __repr__(self) -> str:
+        return f"Parsing(args={self.args!r})"

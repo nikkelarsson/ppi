30f2fac20400b0c74f7dc394a14cfb61fec6b152
Niklas Mikael <nikke.larsson@gmail.com>
2021-09-10 11:11:33 +0300

    Added module "parsing.py".

---
+++ ppi/parsing.py
@@ -0,0 +1,9 @@
+"""
+parsing.py – Gather and parse command line arguments.
+Author: Niklas Larsson
+Date: September 10, 2021
+"""
+
+class Parsing:
+    def __init__(self, args: list) -> None:
+        self.args: list = args

5394b4d602802ea6e818b5b8432de8f6fdc9a285
Niklas Mikael <nikke.larsson@gmail.com>
2021-09-14 12:50:05 +0300

    Added methods for parsing arguments.

--- ppi/parsing.py
+++ ppi/parsing.py
@@ -57,17 +57,36 @@ class ArgParser(BaseParser):
 
         # This section is temporary – it is here for now
         # so that we can test that the program works.
-        for i in self.opts_long:
-            print("Long arg: %s" % i)
-        for i in self.opts_short:
-            print("Short arg: %s" % i)
-        for i in self.pos_args:
-            print("Pos arg: %s" % i)
-        for i in self.invalid_args:
-            print("Inv arg: %s" % i)
+        # for i in self.opts_long:
+        #     print("Long arg: %s" % i)
+        # for i in self.opts_short:
+        #     print("Short arg: %s" % i)
+        # for i in self.pos_args:
+        #     print("Pos arg: %s" % i)
+        # for i in self.invalid_args:
+        #     print("Inv arg: %s" % i)
 
     def __repr__(self) -> str:
         return f"ArgParser(args={self.args!r})"
 
     def __str__(self) -> str:
         return f"Args: {self.args[1:]}"
+
+    def parse_args_short(self) -> None:
+        for arg in self.opts_short:
+            for index, letter in enumerate(arg):
+                if index == 0:
+                    continue  # Skip the "-" prefix.
+                for flag in VALID_FLAGS["short"].keys():
+                    if letter == flag:
+                        pass
+
+    def parse_args_long(self) -> None:
+        for arg in self.opts_long:
+            pass
+
+    def parse_args(self) -> None:
+        if self.opts_short:
+            self.parse_args_short()
+        if self.opts_long:
+            self.parse_args_long()

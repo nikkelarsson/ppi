14a702f5ae2e790328be908f95313b376b016dc0
Niklas Mikael <nikke.larsson@gmail.com>
2021-10-01 10:46:00 +0300

    Write error function.

--- ppi/errors.py
+++ ppi/errors.py
@@ -13,3 +13,11 @@ def invargerror(lang: str, prname: str, arg: str) -> None:
         print("{}: virhe: virheellinen argumentti '{}'".format(prname, arg))
     elif lang == langcodes.ENGLISH:
         print("{}: error: invalid argument '{}'".format(prname, arg))
+
+
+def direxistserror(lang: str, program: str, arg: str) -> None:
+    """Print error when project folder already exists."""
+    if lang == langcodes.FINNISH:
+        print("{}: virhe: kansio '{}' on jo olemassa".format(program, arg))
+    elif lang == langcodes.ENGLISH:
+        print("{}: error: dir '{}' already exists".format(program, arg))

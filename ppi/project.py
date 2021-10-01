40558682ed4dac7a7058bd3356666c7b64c14d60
Niklas Mikael <nikke.larsson@gmail.com>
2021-10-01 10:58:32 +0300

    Finish func "makesetup".

--- ppi/project.py
+++ ppi/project.py
@@ -19,7 +19,19 @@ def makedir(lang: str, program: str, name: str) -> None:
 
 def makesetup(name: str) -> None:
     """Create setup.py -file."""
-    pass
+    with open("setup.py", "w", encoding="utf-8") as setup_py:
+        setup_py.write("from setuptools import setup")
+        setup_py.write("setup(")
+        setup_py.write("\tname=\"{}\",".format(name))
+        setup_py.write("\tversion=\"1.0\",")
+        setup_py.write("\tdescription=\"\",")
+        setup_py.write("\tkeywords=\"\",")
+        setup_py.write("\tauthor=\"\",")
+        setup_py.write("\tpackages=[\"{}\"],".format(name))
+        setup_py.write("\tentry_points={\"console_scripts\": [\"\"]},")
+        setup_py.write("\tinclude_package_data=True,")
+        setup_py.write("\tzip_safe=False")
+        setup_py.write("\t)")
 
 
 def create(lang: str, program: str, prname: str) -> None:

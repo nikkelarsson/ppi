49768d946cc72b52c7beb9fdf41b5582d893a2b7
Niklas Mikael <nikke.larsson@gmail.com>
2021-10-01 09:38:46 +0300

    Add docstrings.

--- ppi/project.py
+++ ppi/project.py
@@ -6,12 +6,15 @@ Date: September 30, 2021
 
 
 def makedir(name: str) -> None:
+    """Create dirs for a project and it's sourcecode."""
     pass
 
 
 def makesetup(name: str) -> None:
+    """Create setup.py -file."""
     pass
 
 
 def create(prname: str) -> None:
+    """Create everything."""
     pass

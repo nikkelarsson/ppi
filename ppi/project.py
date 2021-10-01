b481b1fb4b33e39e8c02dcf76a9061b939150174
Niklas Mikael <nikke.larsson@gmail.com>
2021-10-01 10:48:30 +0300

    Add "guard" check + couple of imports.

--- ppi/project.py
+++ ppi/project.py
@@ -4,11 +4,16 @@ Author: Niklas Larsson
 Date: September 30, 2021
 """
 
+from . import errors
 import os
+import sys
 
 
-def makedir(name: str) -> None:
+def makedir(lang: str, program: str, name: str) -> None:
     """Create dirs for a project and it's sourcecode."""
+    if os.path.exists(name):
+        errors.direxistserror(lang, program, name)
+        sys.exit(1)
     os.makedirs("{0}/{0}".format(name), exist_ok=True)
 
 
@@ -17,6 +22,6 @@ def makesetup(name: str) -> None:
     pass
 
 
-def create(prname: str) -> None:
+def create(lang: str, program: str, prname: str) -> None:
     """Create everything."""
-    pass
+    makedir(lang, program, prname)

a1dd24867b310dc079dfbc398f05870bbb23152c
Niklas Mikael <nikke.larsson@gmail.com>
2021-10-02 16:58:21 +0300

    Update help pages.

--- ppi/hpages.py
+++ ppi/hpages.py
@@ -10,9 +10,11 @@ from . import langcodes
 def show(name: str, lang: str) -> None:
     if lang == langcodes.FINNISH:
         print("\nValitsimet:")
+        print("-q,  --quiet...... Älä tulosta mitään stdout:iin.")
         print("-h,  --help....... Tulosta tämä viesti.")
         print("-V,  --version.... Tulosta {} versio.".format(name))
     elif lang == langcodes.ENGLISH:
         print("\nOptions:")
+        print("-q,  --quiet...... Don't print anything to stdout.")
         print("-h,  --help....... Print this message.")
         print("-V,  --version.... Print {} version.".format(name))

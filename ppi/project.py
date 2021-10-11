300a38ae132b4c5a05984afdd06e4b03cb98107f
Niklas Mikael <nikke.larsson@gmail.com>
2021-10-11 11:48:29 +0300

    Add man-pages feature.

--- ppi/project.py
+++ ppi/project.py
@@ -5,8 +5,11 @@ Date: September 30, 2021
 """
 
 from . import errors
+import datetime as dt
 import os
 import sys
+
+ENC: str = "utf-8"
 
 
 def makedir(lang: str, program: str, name: str) -> None:
@@ -19,7 +22,7 @@ def makedir(lang: str, program: str, name: str) -> None:
 
 def makesetup(name: str) -> None:
     """Create setup.py -file."""
-    with open("{}/setup.py".format(name), "w", encoding="utf-8") as setup_py:
+    with open("{}/setup.py".format(name), "w", encoding=ENC) as setup_py:
         script: str = "{0}={0}.main:main".format(name)
         setup_py.write("from setuptools import setup\n\n")
         setup_py.write("setup(\n")
@@ -37,19 +40,19 @@ def makesetup(name: str) -> None:
 
 def makeinit(name: str) -> None:
     """Create __init__.py file to the sourcecode dir."""
-    with open("{0}/{0}/__init__.py".format(name), "w", encoding="utf-8") as initfile:
+    with open("{0}/{0}/__init__.py".format(name), "w", encoding=ENC) as initfile:
         initfile.write("")  # Just "touch" the file.
 
 
 def makereadme(name: str) -> None:
     """Create a README -file."""
-    with open("{}/README.md".format(name), "w", encoding="utf-8") as readme:
+    with open("{}/README.md".format(name), "w", encoding=ENC) as readme:
         readme.write("# {}\n".format(name))
 
 
 def makemain(name: str) -> None:
     """Create main.py -file."""
-    with open("{0}/{0}/main.py".format(name), "w", encoding="utf-8") as main:
+    with open("{0}/{0}/main.py".format(name), "w", encoding=ENC) as main:
         main.write("\"\"\"\n")
         main.write("main.py\n")
         main.write("Author:\n")
@@ -63,11 +66,37 @@ def makemain(name: str) -> None:
         main.write("    pass\n")
 
 
+def makedocsdir(projectname: str) -> None:
+    """Create dir for docs like man-pages."""
+    os.makedirs("{0}/docs".format(projectname), exist_ok=True)
+
+
+def makemanpages(projectname: str) -> None:
+    """Create basic man-pages skeleton."""
+    month: str = dt.datetime.now().strftime("%b")
+    year: str = dt.datetime.now().strftime("%Y")
+    with open("{0}/docs/{0}.1.md".format(projectname), "w", encoding=ENC) as manp:
+        manp.write("% {0}(1) {1} 0.1\n".format(projectname.upper(), projectname))
+        manp.write("% Author's name here\n")
+        manp.write("% {0} {1}\n\n".format(month, year))
+        manp.write("# NAME\n")
+        manp.write("{0} -- Short description of the program.\n\n".format(projectname))
+        manp.write("# SYNOPSIS\n")
+        manp.write("**{0}** \[*OPT\_SHORT* | *OPT\_LONG*\]\n\n".format(projectname))
+        manp.write("# DESCRIPTION\n")
+        manp.write("More detailed description of the program.\n\n")
+        manp.write("# OPTIONS\n")
+        manp.write("**OPT\_SHORT** | **OPT\_LONG**\n")
+        manp.write(": Description about flag(s).\n")
+
+
 def create(lang: str, program: str, prname: str) -> None:
     """Create everything."""
     makedir(lang, program, prname)
 
     # Stuff to create inside the "root" dir.
+    makedocsdir(prname)
+    makemanpages(prname)
     makereadme(prname)
     makesetup(prname)
 

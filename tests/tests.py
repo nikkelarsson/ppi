6a483e7bc21e3a99006373725164ce745bc1ff9c
Niklas Mikael <nikke.larsson@gmail.com>
2021-11-04 14:31:31 +0200

    Work in progress: update tests.

--- tests/tests.py
+++ tests/tests.py
@@ -1,5 +1,7 @@
 from ppi import langcodes
 from ppi import main
+from ppi import project
+from ppi import parsing
 import unittest as ut
 
 
@@ -27,6 +29,48 @@ class OSTestCase(ut.TestCase):
                 "darwin" or "linux",
                 f"{main.NAME} is only compatible with macOS and Linux."
                 )
+
+
+class BasicEvalMethodsTestCase(ut.TestCase):
+    def setUp(self) -> None:
+        self.methods: object = parsing.BasicEvalMethods()
+
+    def test_output_correctness(self) -> None:
+        """Test that the output is as expected."""
+        for i in range(1, 100):
+            with self.subTest(prefix="-"*i):
+                self.assertTrue(
+                        self.methods.startswith_hyphens(
+                            "{0}test".format("-"*i), i
+                            )
+                        )
+
+
+class ArgparserTestCase(ut.TestCase):
+    def setUp(self) -> None:
+        """Set up some init values for testing."""
+        self.args_inp: list = ["test", "-h", "--test", "---invalid", "-V"]
+        self.name_inp: str = "name"
+        self.version_inp: str = "version"
+        self.lang_inp: str = "lang"
+        self.argparser: object = parsing.ArgParser(
+                self.args_inp,
+                self.name_inp,
+                self.version_inp,
+                self.lang_inp
+                )
+        self.argparser.sort_args()
+
+    def test_input_registration(self) -> None:
+        """Test that inputs are registered properly."""
+        self.assertEqual(self.argparser.args, self.args_inp)
+        self.assertEqual(self.argparser.name, self.name_inp)
+        self.assertEqual(self.argparser.version, self.version_inp)
+        self.assertEqual(self.argparser.lang, self.lang_inp)
+
+    def test_arg_sorting(self) -> None:
+        """Test that args are sorted correctly."""
+        self.assertEqual(self.argparser.invalid_args, ["---invalid"])
 
 
 if __name__ == "__main__":

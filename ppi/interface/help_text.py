914d259b7a9937e11483afcd87b286a9643bb897
Niklas Mikael <nikke.larsson@gmail.com>
2021-11-13 18:17:30 +0200

    Continue updating file imports etc.

--- ppi/interface/help_text.py
+++ ppi/interface/help_text.py
@@ -4,11 +4,11 @@ Author: Niklas Larsson
 Date: September 30, 2021
 """
 
-from ppi import static
+from ppi.static import langcodes
 
 
 def show(name: str, lang: str) -> None:
-    if lang == static.langcodes.LANGCODES["FINNISH"]:
+    if lang == langcodes.LANGCODES["FINNISH"]:
         print("\nValitsimet:")
         print("-q,  --quiet...... Älä tulosta mitään stdout:iin.")
         print("-i,  --git-init... Alusta projekti git-repona.")
--- ppi/interface/success_text.py
+++ ppi/interface/success_text.py
@@ -4,12 +4,12 @@ Author: Niklas Larsson
 Date: October 1, 2021
 """
 
-from ppi import static
+from ppi.static import langcodes
 
 
 def msg(lang: str, program: str, prname: str) -> None:
     """Print success message."""
-    if lang == static.langcodes.LANGCODES["FINNISH"]:
+    if lang == langcodes.LANGCODES["FINNISH"]:
         print("{}: \"{}\" luotu.".format(program, prname))
     if lang.startswith("en_") or lang is None:
         print("{}: \"{}\" created.".format(program, prname))

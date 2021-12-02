754facf30d026e57abcccb3ff039072b12425db9
Niklas Mikael <nikke.larsson@gmail.com>
2021-12-02 18:16:25 +0200

    Bump up the version

--- ppi/main.py
+++ ppi/main.py
@@ -14,7 +14,7 @@ from ppi import parsing
 LANG: str = os.getenv("LANG")
 NAME: str = "ppi"
 MAJOR: int = 1
-MINOR: int = 1
+MINOR: int = 2
 PATCH: int = 0
 VERSION: str = "{}.{}.{}".format(MAJOR, MINOR, PATCH)
 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="ppi",
-    version="1.1.0",
+    version="1.2.0",
     description="Simple utility to create new Python -projects with.",
 
     classifiers=[

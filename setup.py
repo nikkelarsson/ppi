95503f119ff7c8a64d516b38ca8a9b127fcc0876
Niklas Mikael <nikke.larsson@gmail.com>
2022-01-02 16:04:13 +0200

    setup.py: add necessary stuff (for PyPI)

--- setup.py
+++ setup.py
@@ -1,30 +1,35 @@
 from setuptools import setup
 
+
+def readme() -> None:
+    """Long description."""
+    with open("README.md", "r", encoding="utf-8") as f:
+        return f.read()
+
+
 setup(
     name="ppi",
     version="1.2.1",
     description="Simple utility to create new Python -projects with.",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    keywords="automation utility",
+    url="https://github.com/nikkelarsson/ppi",
+    author="Niklas Larsson",
 
     classifiers=[
-        "Environment :: Console"
+        "Environment :: Console",
         "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
         "Operating System :: POSIX :: Linux",
         "Operating System :: MacOS",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
-        "Topic :: Software Development :: Build Tools",
+        "Programming Language :: Python :: 3.10",
     ],
 
-    keywords="utility",
-    url="https://github.com/nikkelarsson/ppi",
-    author="Niklas Larsson",
-
-    packages=[
-        "ppi",
-        "ppi.interface",
-        "ppi.static"
-    ],
-
+    packages=["ppi", "ppi.interface", "ppi.static"],
+    python_requires=">=3.8",  # This parameter requires setuptools >=24.2.0
     install_requires=["colorama"],
     entry_points={"console_scripts": ["ppi=ppi.main:main"]},
     include_package_data=True,
